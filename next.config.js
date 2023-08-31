8f72a4464e7e8d13817a5b1bdff3579808a4e878
Emon <emon.mhk69@gmail.com>
2023-08-31 11:48:23 +0600

    image issue fixed

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
         },
         {
           protocol: "https",
+          hostname: "ibb.co",
+        },
+        {
+          protocol: "https",
           hostname: "images.unsplash.com",
         },
         {
